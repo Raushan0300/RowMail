70d51a8747d48c3be805b53bbc3aaebdc507a2c7
Raushan Kumar <raushankumar2909@gmail.com>
2024-08-19 20:03:52 +0530

    chore: Update CORS configuration to allow credentials from client URL
    This commit updates the CORS configuration in the `index.js` file to allow credentials from the client URL specified in the `process.env.CLIENT_URL` environment variable. By setting the `origin` to the client URL and enabling `credentials`, the server will now accept requests with cookies from the client.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (`chore` for miscellaneous tasks).

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
